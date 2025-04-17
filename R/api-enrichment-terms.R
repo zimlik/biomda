f748b4df23512c3e419c26e67e7829d8f7f480e7
zimlik <2194996299@qq.com>
2025-04-17 10:19:11 +0800

    update function to download terms of biomda

--- R/api-enrichment-terms.R
+++ R/api-enrichment-terms.R
@@ -6,14 +6,17 @@ get_biomda_terms <- function() {
   if (!exists("biomda_terms", envir = biomda_env, inherits = FALSE)) {
     url <- "http://39.108.209.221/biomda-enrichment-terms.rds"
     terms_data <- tempfile(fileext = ".rds")
-    on.exit(file.remove(terms_data))
+    on.exit({
+      if (file.exists(terms_data)) {
+        file.remove(terms_data)
+      }
+    })
     download.file(url, terms_data)
-    biomda_terms <- tryCatch(readRDS(terms_data), error = function(e) NULL)
-    if (is.null(biomda_terms)) {
-      stop("Enrichment terms download failed!")
-    } else {
-      assign("biomda_terms", biomda_terms, envir = biomda_env)
-    }
+    biomda_terms <- tryCatch(
+      readRDS(terms_data),
+      error = function(e) stop("Enrichment terms download failed!")
+    )
+    assign("biomda_terms", biomda_terms, envir = biomda_env)
   }
   biomda_terms <- get("biomda_terms", envir = biomda_env)
   return(biomda_terms)
