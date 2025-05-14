5b5b9e246d182541d5fa0384520ae4de19da97ba
zimlik <smu18575877413@gmail.com>
2025-05-14 11:22:52 +0800

    update ssimcid_search

--- R/api-ssim-compound.R
+++ R/api-ssim-compound.R
@@ -1,12 +1,18 @@
 #' @title Structurally similar compounds
 #' @description Search compounds shared similarity structure.
 #' @param cid PubChem cid, e.g., CID2244.
+#' @param n The number of structurally analogous compounds was analyzed, with
+#' missing values indicating retention of all entries in the dataset. The order
+#' see \code{details}.
 #' @param include_query Whether the structurally similar compounds contains the
 #' query cid.
 #' @importFrom dplyr relocate filter
 #' @details \code{ssimcid_search} is based on substructure key-based 2D
 #' Tanimoto similarity search (tanimoto >= 90%). The synonyms of compound can be
-#' converted to "cid" (PubChem cid) using \code{\link{compound2cid}}.
+#' converted to "cid" (PubChem cid) using \code{\link{compound2cid}}. The
+#' \code{ssimcid} are sorted by "annotation order", a sort of measure about how
+#' much is known about a compound based on how much.
+#' information we have about it.
 #' @return A tibble containing the queried cid and ssimcid (compounds shared
 #' structural similarity in 2D)
 #' @examples
@@ -15,7 +21,7 @@
 #' cid2ssimcid <- ssimcid_search(cid)
 #' cid2ssimcid$ssimcid
 #' @export
-ssimcid_search <- function(cid, include_query = TRUE) {
+ssimcid_search <- function(cid, include_query = TRUE, n) {
   cid <- sub("^CID", "", cid)
   init_cid2ssimcid <- list(cid = cid,
                            ssimcid = rep(NA_character_, length(cid)))
@@ -32,6 +38,12 @@ ssimcid_search <- function(cid, include_query = TRUE) {
       as_tibble()
     if (!include_query) {
       cid2ssimcid <- filter(cid2ssimcid, cid != ssimcid)
+    }
+    if (!missing(n)) {
+      cid2ssimcid <- cid2ssimcid |>
+        group_by(cid) |>
+        slice_head(n = n) |>
+        ungroup()
     }
   } else {
     cid2ssimcid <- as_tibble(init_cid2ssimcid)
