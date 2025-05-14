6f2b7a438fefd7ac25a3606b65e124e14197e7fe
zimlik <smu18575877413@gmail.com>
2025-05-14 11:05:59 +0800

    update ssimcid_search

--- R/api-ssim-compound.R
+++ R/api-ssim-compound.R
@@ -1,6 +1,8 @@
 #' @title Structurally similar compounds
 #' @description Search compounds shared similarity structure.
 #' @param cid PubChem cid, e.g., CID2244.
+#' @param include_query Whether the structurally similar compounds contains the
+#' query cid.
 #' @importFrom dplyr relocate filter
 #' @details \code{ssimcid_search} is based on substructure key-based 2D
 #' Tanimoto similarity search (tanimoto >= 90%). The synonyms of compound can be
@@ -13,7 +15,7 @@
 #' cid2ssimcid <- ssimcid_search(cid)
 #' cid2ssimcid$ssimcid
 #' @export
-ssimcid_search <- function(cid) {
+ssimcid_search <- function(cid, include_query = TRUE) {
   cid <- sub("^CID", "", cid)
   init_cid2ssimcid <- list(cid = cid,
                            ssimcid = rep(NA_character_, length(cid)))
@@ -22,12 +24,15 @@ ssimcid_search <- function(cid) {
     cid2ssimcid <- resp$cid2ssimcid |>
       separate_longer_delim(ssimcid, delim = ",") |>
       left_join(resp$cnodes, by = c("ssimcid" = "id")) |>
-      filter(!is.na(name), cid != ssimcid) |>
+      filter(!is.na(name)) |>
       mutate(cid = paste0("CID", cid),
              ssimcid = paste0("CID", ssimcid),
              evidence = "tanimoto >= 90%") |>
       relocate(evidence, .before = name) |>
       as_tibble()
+    if (!include_query) {
+      cid2ssimcid <- filter(cid2ssimcid, cid != ssimcid)
+    }
   } else {
     cid2ssimcid <- as_tibble(init_cid2ssimcid)
   }
